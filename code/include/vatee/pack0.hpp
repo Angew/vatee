2f631ca95fa4c230e2299b46f1b6eed7d9b70be7
Petr Kmoch <petr.kmoch@gmail.com>
2014-01-28 23:10:47 +0100

    WIP on basic test
    Renamed to basic0, using pack0
    Public headers for pack0 and punctuation

--- code/include/vatee/pack0.hpp
+++ code/include/vatee/pack0.hpp
@@ -1,5 +1,17 @@
-#ifndef
-#define
+/*
+Copyright Petr Kmoch 2014.
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file ../../../LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#ifndef VATEE_GUARD_PACK0
+#define VATEE_GUARD_PACK0
+
+#include "vatee/internal/compiler_version.hpp"
+
+#include "vatee/internal/expansion.hpp"
+#include "vatee/internal/facility.hpp"
+
 
 #define VATEE_EXPAND_0_AFTER_1(vatee_arg0) \
 	VATEE_INTERNAL_EXPAND_MACRO(VATEE_PACKSIZE_0, 2) (vatee_arg0, VATEE_INTERNAL_EMPTY)
@@ -7,8 +19,11 @@
 #define VATEE_EXPAND_0_AFTER_2(vatee_arg0, vatee_arg1) \
 	VATEE_INTERNAL_EXPAND_MACRO(VATEE_PACKSIZE_0, 3) (vatee_arg0, vatee_arg1, VATEE_INTERNAL_EMPTY)
 
-#define VATEE_TRAILING_EXPAND_0_AFTER_1(vatee_arg0, vatee_arg1) \
-	VATEE_VARIADIC_0_COMMA VATEE_INTERNAL_EXPAND_MACRO(VATEE_PACKSIZE_0, 2) (vatee_arg0, VATEE_INTERNAL_EMPTY)
+#define VATEE_TRAILING_EXPAND_0_AFTER_1(vatee_arg0) \
+	VATEE_NONEMPTY_0_COMMA VATEE_INTERNAL_EXPAND_MACRO(VATEE_PACKSIZE_0, 2) (vatee_arg0, VATEE_INTERNAL_EMPTY)
+
+#define VATEE_TRAILING_EXPAND_0_AFTER_2(vatee_arg0, vatee_arg1) \
+	VATEE_NONEMPTY_0_COMMA VATEE_INTERNAL_EXPAND_MACRO(VATEE_PACKSIZE_0, 3) (vatee_arg0, vatee_arg1, VATEE_INTERNAL_EMPTY)
 
 #define VATEE_EXPAND_0_BETWEEN_2(vatee_arg0, vatee_arg1) \
 	VATEE_INTERNAL_EXPAND_MACRO(VATEE_PACKSIZE_0, 2) (vatee_arg0, vatee_arg1 VATEE_INTERNAL_EMPTY)
@@ -16,4 +31,7 @@
 #define VATEE_EXPAND_0_BETWEEN_3(vatee_arg0, vatee_arg1, vatee_arg2) \
 	VATEE_INTERNAL_EXPAND_MACRO(VATEE_PACKSIZE_0, 3) (vatee_arg0, vatee_arg1, vatee_arg2 VATEE_INTERNAL_EMPTY)
 
-#endif
+#define VATEE_TEMPLATE_0(vatee_arg0) \
+	VATEE_IF_NONEMPTY_0(template <) VATEE_INTERNAL_EXPAND_MACRO(VATEE_PACKSIZE_0, 2) (vatee_arg0, VATEE_INTERNAL_EMPTY) VATEE_IF_NONEMPTY_0(>)
+
+#endif	//VATEE_GUARD_PACK0
---
+++ test/basic0.cpp
@@ -0,0 +1,39 @@
+/*
+Copyright Petr Kmoch 2014.
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file ../LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#include "vatee/pack0.hpp"
+#include "vatee/punctuation.hpp"
+
+#include <iostream>
+
+
+template <class T, class U>
+const T& as(const U &a)
+{ return a; }
+
+
+#define VATEE_VARIADIC \
+	template <class T VATEE_TRAILING_EXPAND_0_AFTER_1(class T)> \
+	void print(const T &a VATEE_TRAILING_EXPAND_0_AFTER_2(const T, &a)) { \
+		std::cout << a << '\n'; \
+		VATEE_IF_NONEMPTY_0(print(VATEE_EXPAND_0_AFTER_1(a));) \
+	}
+#include VATEE_EMULATE_0()
+
+
+#define VATEE_VARIADIC \
+	VATEE_TEMPLATE_0(class T) \
+	void test(VATEE_EXPAND_0_AFTER_2(const T, &a)) { \
+		print(VATEE_EXPAND_0_AFTER_1(a)); \
+		print(VATEE_EXPAND_0_BETWEEN_3(as<T, >VATEE_LPAREN a, VATEE_RPAREN)); \
+	}
+#include VATEE_EMULATE_0()
+
+
+int main()
+{
+	test(1, 2.5, 'a', "s");
+}
